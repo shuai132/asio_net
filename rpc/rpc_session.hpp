ce93c78e6a24e8448a63424c13d932298cab7145
liushuai <770722922@qq.com>
2022-09-07 21:01:46 +0800

    feat: support rpc_session close

--- rpc/rpc_session.hpp
+++ rpc/rpc_session.hpp
@@ -1,19 +1,33 @@
 #pragma once
 
+#include <utility>
+
 #include "RpcCore.hpp"
 #include "noncopyable.hpp"
+#include "tcp_server.hpp"
 
 namespace asio_net {
 
 class rpc_session : noncopyable {
  public:
-  explicit rpc_session() = default;
+  explicit rpc_session(std::weak_ptr<tcp_session> tcp_session) : tcp_session_(std::move(tcp_session)) {}
+
+ public:
+  void close() {
+    auto ts = tcp_session_.lock();
+    if (ts) {
+      ts->close();
+    }
+  }
 
  public:
   std::function<void()> on_close;
 
  public:
   std::shared_ptr<RpcCore::Rpc> rpc;
+
+ private:
+  std::weak_ptr<tcp_session> tcp_session_;
 };
 
 }  // namespace asio_net
