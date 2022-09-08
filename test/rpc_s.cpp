35b37b01c1bb10dccc279934b1eb719645ca01a9
liushuai <770722922@qq.com>
2022-09-08 11:21:44 +0800

    test: print rpc session ptr

--- test/rpc_s.cpp
+++ test/rpc_s.cpp
@@ -10,10 +10,10 @@ int main(int argc, char** argv) {
   asio::io_context context;
   rpc_server server(context, PORT);
   server.on_session = [](const std::weak_ptr<rpc_session>& rs) {
-    printf("on_session:\n");
     auto session = rs.lock();
-    session->on_close = [] {
-      printf("session on_close:\n");
+    printf("on_session: %p\n", session.get());
+    session->on_close = [rs] {
+      printf("session on_close: %p\n", rs.lock().get());
     };
     session->rpc->subscribe<RpcCore::String, RpcCore::String>("cmd", [](const RpcCore::String& data) {
       printf("session on cmd: %s\n", data.c_str());
