b073569d7a551b3b2ac879b1277761f4227a93b4
liushuai <770722922@qq.com>
2024-11-18 21:59:34 +0800

    feat: refine result type

--- test/rpc_c_coroutine.cpp
+++ test/rpc_c_coroutine.cpp
@@ -20,21 +20,21 @@ int main() {
         [rpc]() -> asio::awaitable<void> {
           // std::string
           auto rsp1 = co_await rpc->cmd("cmd")->msg(std::string("hello"))->async_call<std::string>();
-          LOG("string: type: %s: data: %s", rpc_core::request::finally_t_str(rsp1.type), rsp1.data.c_str());
+          LOG("string: type: %s: data: %s", rpc_core::finally_t_str(rsp1.type), (*rsp1).c_str());
           // void
           auto rsp2 = co_await rpc->cmd("cmd")->msg(std::string("hello"))->async_call();
-          LOG("void: type: %s", rpc_core::request::finally_t_str(rsp2.type));
+          LOG("void: type: %s", rpc_core::finally_t_str(rsp2.type));
 
           /// custom async call
           // string
           auto rsp3 = co_await rpc->cmd("cmd")->msg(std::string("hello"))->async_custom<std::string>();
-          LOG("custom: string: type: %s: data: %s", rpc_core::request::finally_t_str(rsp3.type), rsp3.data.c_str());
+          LOG("custom: string: type: %s: data: %s", rpc_core::finally_t_str(rsp3.type), rsp3.data.c_str());
           // void
           auto rsp4 = co_await rpc->cmd("cmd")->msg(std::string("hello"))->async_custom();
-          LOG("custom: void: type: %s", rpc_core::request::finally_t_str(rsp4.type));
+          LOG("custom: void: type: %s", rpc_core::finally_t_str(rsp4.type));
           // cancel
           auto rsp5 = co_await rpc->cmd("cmd")->msg(std::string("hello"))->cancel()->async_custom();
-          LOG("custom: cancel: type: %s", rpc_core::request::finally_t_str(rsp5.type));
+          LOG("custom: cancel: type: %s", rpc_core::finally_t_str(rsp5.type));
         },
         asio::detached);
   };
